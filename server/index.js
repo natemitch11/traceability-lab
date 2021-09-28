9029312952599fb033a9ae43dc26c7354e4de00b
natemitch11 <nate.mitch11@gmail.com>
2021-09-28 14:27:42 -0600

    troubleshooting error for rollbar

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const path = require('path')
 const app = express()
 
-app.use(express.static('client'))
+// app.use(express.static('client'))
 
 const Rollbar = require('rollbar')
 const rollbar = new Rollbar({
