0a2fa485cbdf679a32c631401e2e19264b7c8625
natemitch11 <nate.mitch11@gmail.com>
2021-09-28 15:29:39 -0600

    more css troubleshoots

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const path = require('path')
 const app = express()
 
-app.use(express.static(__dirname + '/public'));
+app.use('/static', express.static(path.join(__dirname, 'public')))
 app.use(express.json())
 
 const Rollbar = require('rollbar')
