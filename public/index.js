9a39571d5f4f52b3a335f0184bee07802765dc1c
natemitch11 <nate.mitch11@gmail.com>
2021-09-28 16:11:29 -0600

    axios fix

--- public/index.js
+++ public/index.js
@@ -3,15 +3,14 @@ console.log('hello there')
 let baseURL = 'https://traceability-lab-yeah.herokuapp.com'
 let button = document.querySelector('button')
 
-function criticalBtn() {
-    console.log('click')
+
+button.addEventListener('click', ()=>{
+   console.log('click')
     axios.get(baseURL + '/critical')
     .then(res => {
         console.log(res.body)
     })
     .catch(err =>{
         console.error(err)
-    })
-}
-
-button.addEventListener('click', criticalBtn())
+    }) 
+})
