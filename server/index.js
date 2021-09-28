9fcfad79a073c1c91cad6675b37bf4ea9fdfbea3
natemitch11 <nate.mitch11@gmail.com>
2021-09-28 14:25:47 -0600

    added new access token

--- server/index.js
+++ server/index.js
@@ -2,14 +2,14 @@ const express = require('express')
 const path = require('path')
 const app = express()
 
+app.use(express.static('client'))
+
 const Rollbar = require('rollbar')
 const rollbar = new Rollbar({
-    accessToken: '80fa49bd6edf4db78eb1fb61a73f0694',
+    accessToken: '3632c213ce864b659af5aa3439cd2e27',
     captureUncaught: true,
     captureUnhandledRejections: true
 });
-
-app.use(express.static('client'))
 
 app.get('/', (req, res)=>{
     res.sendFile(path.join(__dirname, '../client/index.html'))
